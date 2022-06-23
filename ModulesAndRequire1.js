372194c712193444a5ce87e23ebb078e7d4969ae
sahilburle <sahilburle1998@gmail.com>
2022-06-23 12:10:54 +0530

    some modification

--- ModulesAndRequire1.js
+++ ModulesAndRequire1.js
@@ -4,4 +4,21 @@ var counter = function(arr){
 
 //console.log(counter(['shaun', 'crystal', 'ryu']));
 
-module.exports = counter;
+var adder = function(a,b) {
+    return `The sum of the 2 numbers is ${a + b}`;
+};
+
+var pi = 3.142
+
+// module.exports.counter = counter;
+// module.exports.adder = adder;
+// module.exports.pi = pi;
+
+module.exports = {
+    conter: counter,
+    adder: adder,
+    pi: pi
+};
+
+//Module exports are the instruction that tells Node. js which bits of code (functions, objects, strings, etc.) 
+//to “export” from a given file so other files are allowed to access the exported code.
--- ModulesAndRequires2.js
+++ ModulesAndRequires2.js
@@ -1,3 +1,15 @@
-var counter = require('./ModulesAndRequire1');
+//var counter = require('./ModulesAndRequire1');
 
-console.log(counter(['shaun', 'crystal', 'ryu']));
+//require() is a built-in function to include external modules that exist in separate files.
+//require() statement basically reads a JavaScript file, 
+//executes it, and then proceeds to return the export object.
+
+var stuff = require('./ModulesAndRequire1'); 
+
+//console.log(counter(['shaun', 'crystal', 'ryu']));
+
+console.log(stuff.counter(['shaun', 'crystal', 'ryu']));
+
+console.log(stuff.adder(5,6));
+
+console.log(stuff.adder(stuff.pi, 5));
