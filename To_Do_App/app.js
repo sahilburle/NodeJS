98b384cf36bb6d8658e45900bb955cfaba6caf4b
sahilburle <sahilburle1998@gmail.com>
2022-06-29 14:16:59 +0530

    to-do app

--- To_Do_App/app.js
+++ To_Do_App/app.js
@@ -1,4 +1,6 @@
 var express = require('express');
+const todocontroller = require('./controllers/todocontroller');
+var todoCOntroller = require('./controllers/todocontroller');
 
 var app = express();
 
@@ -8,5 +10,9 @@ app.set('view engine', 'ejs');
 //static files
 app.use(express.static('./public'))
 
+//fire controllers
+todocontroller();
+
 //listen to port
-app.listen(3000)
+app.listen(3000);
+console.log('u r listening to 3000');
