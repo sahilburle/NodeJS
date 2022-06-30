e04a02c76982f99f74e5b606cdc4b624b58bc2cb
sahilburle <sahilburle1998@gmail.com>
2022-06-30 16:14:18 +0530

    to-do app

--- To_Do_App/controllers/todocontroller.js
+++ To_Do_App/controllers/todocontroller.js
@@ -1,4 +1,7 @@
+var bodyParser = require('body-parser');
+
 var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
+var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app){
 
@@ -8,12 +11,17 @@ module.exports = function(app){
 
     });
 
-    app.post('/todo', function(req, res){
+    app.post('/todo',urlencodedParser, function(req, res){
         
+        data.push(req.body);
+        res.json(data);
     });
 
-    app.delete('/todo', function(req, res){
+    app.delete('/todo/;item', function(req, res){
 
+        data = data.filter(function(todo){
+            return todo.item.replace(/ /g, '-') !== req.params.item;
+        });
     });
     
 };
