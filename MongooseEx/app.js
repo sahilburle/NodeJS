2d8e0a48f3f6d1d25c4bf31d3e724e4755df7dae
sahilburle <sahilburle1998@gmail.com>
2022-07-01 12:00:48 +0530

    ..

--- MongooseEx/app.js
+++ MongooseEx/app.js
@@ -2,6 +2,20 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var Book = require('./book.model');
+
+var db = 'mongodb://localhost/example';
+
+mongoose.connect(db);
+
+app.get('/', function(req, res) {
+    res.send('happy to be  here');
+});
+
+app.get('/books', function(req, res) {
+    console.log('getting all books');
+    Book.find({})
+});
 
 var port = 8080;
 
